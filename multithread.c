9a4e56f2cd76ae930d78a3a49679a646d4be90c9
ChrisH456 <122757297+ChrisH456@users.noreply.github.com>
2023-01-25 12:05:29 +0000

    Add files via upload

--- multithread.c
+++ multithread.c
@@ -14,23 +14,18 @@ void *maxfunc(void *param);
 
 int main(int argc, char* argv[])
 {
- 
- /* Create thread IDs */
-
-
+/* Create thread IDs */
+pthread_t tid; /* This is where the unique ID for the thread is created. A unique ID is used so that is is easily recognisable and hard to confuse with other threads*/
+pthread_attr_t attr;
 /* Create thread attributes */
-
-
+pthread_attr_init(&attr);
 /* Initialize thread attributes */
-
-
+int pthread_attr_t &attr;
 /* Create threads */
-
+pthread_create(&tid, &attr, avgfunc, minfunc, maxfunc, argv[1]);
 
 /* Wait for threads to exit */
-
-
-
+pthread_join(tid,NULL);
 printf("%d %d %d", avgNum, minNum, maxNum);
 }
 
@@ -39,18 +34,27 @@ void *avgfunc(void *param)
     // The following two lines help you to access the arguments (list of numbers) passed to the program
     char** args = (char**) param;   // args is a pointer to array of pointers to main func arguments argv
     args++;                        // increment the pointer to point to the first number passed
+  
+int main()
+     int i, total;
+     int a[] = {90, 81, 78, 95, 79, 72, 85};
+     int n = 7;
+    
+    total = 0;
 
+    for (i = 0; i < n; i++){
+        total += a[i];
+        sprintf(avgNum);
+    }
+    
     // To get a number from args array, use atoi(*args)
     // to increment the pointer to point to the next number: *args++
- 
     /* Write the code to calculate the average value and store it in avgNum variable */
- 
-
-
+    return [0];
 }
 
 void *minfunc(void *param)
-{
+
     // The following two lines help you to access the arguments (list of numbers) passed to the program
     char** args = (char**) param;   // args is a pointer to array of pointers to main func arguments argv
     args++;                        // increment the pointer to point to the first number passed
@@ -59,12 +63,32 @@ void *minfunc(void *param)
     // to increment the pointer to point to the next number: *args++
     
     /* Write the code to calculate the minimum value and store it in minNum variable */
+ int main()
+ {
+    int a[] = {90, 81, 78, 95, 79 ,72, 85}, minimum, size, c, location = 1;
+    for (c = 0; c < size; c++)
+    minimum = a[0];
+
+    for (c = 1; c < size ; c++)
+    {
+        if (a[c] < minimum)
+        {
+            minimum = a[c]
+            location = c+1
+        }
+    }
+    sprintf(minNum);
+    return [0];
+ }
+
+
  
  
 
-}
+
 
 void *maxfunc(void *param)
+int main()
 {
     // The following two lines help you to access the arguments (list of numbers) passed to the program
     char** args = (char**) param;   // args is a pointer to array of pointers to main func arguments argv
@@ -74,7 +98,18 @@ void *maxfunc(void *param)
     // to increment the pointer to point to the next number: *args++
     
     /* Write the code to calculate the maximum value and store it in maxNum variable */
- 
- 
+ int a[] = {90, 81, 78, 95, 79, 72, 85}, maximum, size, c, location = 0;
+ for (c = 0; c < size; c++)
+ for (c = 1; c < size; c++)
+ if (array[a] > array[location])
+ location = c;
+ sprintf(maxNum);
+ return [0];
+}
 
+{
+    int (maxNum, minNum avgNum)
+    printf("The average is: %d", &avgNum);
+    printf("The minimum is: %d", &minNum);
+    printf("The maximum is: %d", &maxNum)
 }
