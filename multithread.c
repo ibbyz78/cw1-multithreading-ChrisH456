1e34b0029260bf780accab8b5afc51bde1770646
ChrisH456 <122757297+ChrisH456@users.noreply.github.com>
2023-01-27 14:37:10 +0000

    Add files via upload

--- multithread.c
+++ multithread.c
@@ -43,22 +43,19 @@ void *avgfunc(void *param)
     char** args = (char**) param;   // args is a pointer to array of pointers to main func arguments argv
     args++;                        // increment the pointer to point to the first number passed
   
-int main()
-     int i, total;
-     int a[] = {90, 81, 78, 95, 79, 72, 85};
-     int n = 7;
-    
-    total = 0;
-
-    for (i = 0; i < n; i++){
-        total += a[i];
-        sprintf(avgNum);
-    }
     
     // To get a number from args array, use atoi(*args)
     // to increment the pointer to point to the next number: *args++
     /* Write the code to calculate the average value and store it in avgNum variable */
-    return [0];
+ int main()
+ {
+    float a,b,c,d,e,f,g,avg;
+    printf("Please input 7 numbers.");
+    scanf("%f%f%f%f%f%f%f", &a, &b, &c, &d, &e, &f, &g);
+    avg=(a+b+c+d+e+f+g)/7;
+    int avgNum = avg;
+    return 0;
+ }
 }
 
 void *minfunc(void *param)
@@ -71,27 +68,11 @@ void *minfunc(void *param)
     // to increment the pointer to point to the next number: *args++
     
     /* Write the code to calculate the minimum value and store it in minNum variable */
- int main()
- {
-    int a[] = {90, 81, 78, 95, 79 ,72, 85}, minimum, size, c, location = 1;
-    for (c = 0; c < size; c++)
-    minimum = a[0];
-
-    for (c = 1; c < size ; c++)
-    {
-        if (a[c] < minimum)
-        {
-            minimum = a[c]
-            location = c+1
-        }
-    }
-    sprintf(minNum);
-    return [0];
- }
+ 
  
 void *maxfunc(void *param)
 int main()
-{
+
     // The following two lines help you to access the arguments (list of numbers) passed to the program
     char** args = (char**) param;   // args is a pointer to array of pointers to main func arguments argv
     args++;                        // increment the pointer to point to the first number passed
@@ -100,18 +81,4 @@ int main()
     // to increment the pointer to point to the next number: *args++
     
     /* Write the code to calculate the maximum value and store it in maxNum variable */
- int a[] = {90, 81, 78, 95, 79, 72, 85}, maximum, size, c, location = 0;
- for (c = 0; c < size; c++)
- for (c = 1; c < size; c++)
- if (array[a] > array[location])
- location = c;
- sprintf(maxNum);
- return [0];
-}
 
-{
-    int (maxNum, minNum avgNum)
-    printf("The average is: %d", &avgNum);
-    printf("The minimum is: %d", &minNum);
-    printf("The maximum is: %d", &maxNum)
-}
