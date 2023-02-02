66a14a9d33288bcefff541a9ae2e852d636a432a
ChrisH456 <122757297+ChrisH456@users.noreply.github.com>
2023-02-02 11:57:41 +0000

    Add files via upload

--- multithread.c
+++ multithread.c
@@ -20,28 +20,28 @@ int main(int argc, char* argv[])
     pthread_t Min;
     pthread_t Max;
 
-    pthread_attr_t attrmax;
+    pthread_attr_t attrmax; /*These are the identifiers for the thread atrributes that are created later down the code.*/
     pthread_attr_t attrmin;
     pthread_attr_t attravg;
 /* Create thread attributes */
-    pthread_attr_init(&attrmax);
+    pthread_attr_init(&attrmax); /* These are the attributes for the threads. These use the identifer created above.*/
     pthread_attr_init(&attravg);
     pthread_attr_init(&attrmin);
 /* Initialize thread attributes */
-    int pthread_attr_t &attrmax;
+    int pthread_attr_t &attrmax; /*once the ID and attributes are made these liens of code are used to initiailise it and prepare it for use.*/
     int pthread_attr_t &attrmin;
     int pthread_attr_t &attravg;
 /* Create threads */
-    pthread_create(&Avg, &Max, &Min, &attravg, &attrmax, &attrmin);
+    pthread_create(&Avg, &Max, &Min, &attravg, &attrmax, &attrmin); /* This is the creation of the threads that were defined above.*/
     pthread_create(avgfunc, minfunc, maxfunc, argv[1]);
 
 /* Wait for threads to exit */
-    pthread_join(tid,NULL);
-    printf("%d %d %d", avgNum, minNum, maxNum);
+    pthread_join(tid,NULL); 
+    printf("%d %d %d", avgNum, minNum, maxNum);/*this is where the data is collected from the variable above and printed in a human readable format.*/
 }
 
 void *avgfunc(void *param)
-{
+
     // The following two lines help you to access the arguments (list of numbers) passed to the program
     char** args = (char**) param;   // args is a pointer to array of pointers to main func arguments argv
     args++;                        // increment the pointer to point to the first number passed
@@ -52,14 +52,20 @@ void *avgfunc(void *param)
     /* Write the code to calculate the average value and store it in avgNum variable */
  int main()
  {
-    float a,b,c,d,e,f,g,avg;
-    printf("Please input 7 numbers.");
-    scanf("%f%f%f%f%f%f%f", &a, &b, &c, &d, &e, &f, &g);
-    avg=(a+b+c+d+e+f+g)/7;
-    int avgNum = avg;
+    int i,total;
+    int a[] = {args};
+    int n = 7;
+
+    total = 0;
+
+    for(i = 0; i< n; i++) {
+        total += a[i];
+    }
+    sprintf("%f\n,", &avgNum)
     return 0;
- }
+ 
 }
+
 
 void *minfunc(void *param)
 
